bafc828d0508083a7bf6dcf7815154958a689ff7
devstalin24 <devstalin24@gmail.com>
2019-02-06 21:14:32 +0530

    added  list python code

---
+++ list.py
@@ -0,0 +1,22 @@
+# list 
+fruts = ["apple", "Grapes"]
+print(fruts)
+fruts.append("orange")
+print(fruts)
+
+# insert methid 
+fruts.insert(0,"Banana")
+print(fruts)
+
+# to add two list   -   list1 + list2
+# list1.extend(list2)  -  single list 
+# appends  -  two list 
+# delete data from list  pop()
+# del list[]
+# remove list.remove()
+# if apple in fruts 
+# sort() , copy() ,  clear , sorted function 
+
+# list comapre   == , is 
+# join and split methid 
+#min() max() function 
